25e04a66bcde9f97e4a91af48daa3dbf389d9407
Samuel Wright <wsright987@gmail.com>
2022-12-19 17:09:24 -0500

    netlify update

---
+++ functions/api.js
@@ -0,0 +1,53 @@
+// Import dependencies
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const serverless = require('serverless-http')
+
+const router = require('../src/routes/routes')
+const adminRouter = require('../src/routes/adminRoutes');
+
+// Enable .env
+dotenv.config()
+
+// Define Express app
+const app = express();
+
+// Parse JSON into JS objects
+app.use(express.json());
+app.use(bodyParser.json());
+
+// Enable CORS for all requests
+app.use(cors({
+  credentials: true,
+  origin: '*'
+}));
+
+// Add routes
+app.use("/", router)
+//app.use("/admin", adminRouter)
+
+// Netlify 
+app.use('/.netlify/functions/api', router)
+
+// Connect to MongoDB database
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+};
+
+mongoose
+	.connect(process.env.API_ADMIN_URI, connectionParams)
+	.then(() => {
+    console.log("Mongoose ready!")
+	})
+
+// Start server
+const PORT = process.env.PORT || 8080;
+  
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+module.exports.handler = serverless(app);
