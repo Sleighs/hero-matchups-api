753dbb17fd9c00c30ef9eb996cd4815800990cc6
Samuel Wright <wsright987@gmail.com>
2022-12-18 23:30:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,19 +19,21 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
+// Enable CORS for all requests
+app.use(cors({
+  credentials: true,
+  origin: '*'
+}));
+
 // Add headers
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+/*app.use(function (req, res, next) {
+  //res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
   next();
-});
-
-// Enable CORS for all requests
-app.use(cors());
+});*/
 
 // Log HTTP requests
 app.use(morgan('combined'));
